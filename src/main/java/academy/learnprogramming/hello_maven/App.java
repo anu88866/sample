3d9e81b015ee36ea21fca78849cb01e7447a2b70
anu88866 <anilreddyk999@gmail.com>
2019-10-05 19:19:14 +0530

    first commit

---
+++ src/main/java/academy/learnprogramming/hello_maven/App.java
@@ -0,0 +1,13 @@
+package academy.learnprogramming.hello_maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/academy/learnprogramming/hello_maven/HelloMaven.java
@@ -0,0 +1,10 @@
+package academy.learnprogramming.hello_maven;
+
+public class HelloMaven {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
